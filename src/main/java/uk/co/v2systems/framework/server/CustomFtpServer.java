a74cc71243b747fc9295fc2af958dea3c4222c78
buchadep <buchade_p@hotmail.com>
2015-07-01 13:40:51 +0100

    Added CustomFTPServer class

--- src/main/java/uk/co/v2systems/framework/server/CustomFtpServer.java
+++ src/main/java/uk/co/v2systems/framework/server/CustomFtpServer.java
@@ -1,7 +1,97 @@
 package uk.co.v2systems.framework.server;
 
+import org.apache.ftpserver.ConnectionConfigFactory;
+import org.apache.ftpserver.FtpServer;
+import org.apache.ftpserver.FtpServerFactory;
+import org.apache.ftpserver.ftplet.Authority;
+import org.apache.ftpserver.listener.ListenerFactory;
+import org.apache.ftpserver.usermanager.impl.BaseUser;
+import org.apache.ftpserver.usermanager.impl.WritePermission;
+import uk.co.v2systems.framework.utils.Methods;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by I&T Lab User on 26/06/2015.
  */
-public class CustomFtpServer {
+public class CustomFtpServer implements Runnable {
+        static int ftpPort= 2121;
+        static String FTPServerIp = Methods.getLocalIP();
+        static FtpServer server;
+        static String homeDir="c:/";
+        public static volatile boolean stopRequest = false;
+
+        @Override
+        public void run() {
+            this.start();
+        }
+
+        public static void start(){
+            try{
+                FtpServerFactory serverFactory = new FtpServerFactory();
+                ListenerFactory factory = new ListenerFactory();
+                ConnectionConfigFactory connectionConfigFactory = new ConnectionConfigFactory();
+                connectionConfigFactory.setAnonymousLoginEnabled(true);
+                serverFactory.setConnectionConfig(connectionConfigFactory.createConnectionConfig());
+
+                BaseUser user = new BaseUser();
+                user.setName("anonymous");
+                user.setHomeDirectory(homeDir);
+
+                List<Authority> authorities = new ArrayList<Authority>();
+                authorities.add(new WritePermission());
+                user.setAuthorities(authorities);
+                serverFactory.getUserManager().save(user);
+
+                // set the port of the listener
+                factory.setPort(ftpPort);
+                // replace the default listener
+                serverFactory.addListener("default", factory.createListener());
+                // start the server
+                server = serverFactory.createServer();
+                //serverFactory.createServer();
+                server.start();
+
+            }catch(Exception e){
+                Methods.printConditional("Error in method FTPServer.start :- " + e);
+            }
+        }
+
+        public static void stop(){
+            try{
+                if(server!=null && stopRequest==true)
+                    server.stop();
+                System.out.println("FTP Server Stopped!");
+            }catch(Exception e){
+                Methods.printConditional("Error in method FTPServer.stop " + e);
+            }
+
+        }
+//set stopRequest
+    public static void setStopRequest(boolean stop){
+        stopRequest=stop;
+        if(stop==true)
+            CustomFtpServer.stop();
+        }
+//set FTP server home directory
+        public static void setHomeDir(String homeDir) {
+            CustomFtpServer.homeDir = homeDir;
+        }
+//get FTP server home directory
+        public static String getHomeDir() {
+            return homeDir;
+        }
+// set integer FTP server port
+        public static void setFtpPort(int ftpPort) {
+            CustomFtpServer.ftpPort = ftpPort;
+        }
+// get integer FTP server port
+        public static int getFtpPort() {
+            return ftpPort;
+        }
+    // get FTP server IP
+    public static String getFTPServerIp() {
+        return FTPServerIp;
+    }
 }
--- src/main/java/uk/co/v2systems/framework/testClass.java
+++ src/main/java/uk/co/v2systems/framework/testClass.java
@@ -2,6 +2,7 @@ package uk.co.v2systems.framework;
 
 import uk.co.v2systems.framework.database.CustomSqlClient;
 import uk.co.v2systems.framework.http.CustomHttpClient;
+import uk.co.v2systems.framework.server.CustomFtpServer;
 import uk.co.v2systems.framework.shell.CustomSshClient;
 import uk.co.v2systems.framework.shell.CustomTelnetClient;
 import uk.co.v2systems.framework.utils.CustomDate;
@@ -13,20 +14,30 @@ import uk.co.v2systems.framework.utils.Methods;
 public class testClass {
 
         public static void main(String args[]) {
-            //CustomSshClient.connect("172.26.128.26",22,"PBU10","Changeme_14");
-            //CustomSshClient.executeCommand("ls -ltr /apps ",true);
-            //CustomSshClient.executeCommand("ls -ltr /apps ",true);
-            //CustomSshClient.executeCommand("ls -ltr /apps ",true);
+            try {
+                //CustomSshClient.connect("172.26.128.26",22,"PBU10","Changeme_14");
+                //CustomSshClient.executeCommand("ls -ltr /apps ",true);
+                //CustomSshClient.executeCommand("ls -ltr /apps ",true);
+                //CustomSshClient.executeCommand("ls -ltr /apps ",true);
 
-            CustomTelnetClient  c = new CustomTelnetClient();
-            c.connect("10.66.56.9",23,"root","");
-            c.sendCommand("ls  /");
-            c.sendCommand("ls /bin #");
-            //c.sendCommand("sh");
+                CustomFtpServer ftpServer = new CustomFtpServer();
+                ftpServer.setFtpPort(2121);
+                Thread thread = new Thread(ftpServer, "FTP Server Thread - 0");
+                //Thread thread1 = new Thread(ftpServer, "FTP Server Thread - 1");
+                thread.start();
+                //thread1.start();
 
+                CustomTelnetClient c = new CustomTelnetClient();
+                c.connect("10.66.56.9", 23, "root", "");
+                c.sendCommand("ls  /");
+                c.sendCommand("ftpput -v -u anonymous 172.22.19.9  -P 2121 VERSION.gz /VERSION.gz");
+                //c.sendCommand("sh");
 
+                ftpServer.setStopRequest(true);
 
-            //Methods.printConditional(CustomDate.getMJDDate(CustomDate.getLongDateTime()));
-
+                //Methods.printConditional(CustomDate.getMJDDate(CustomDate.getLongDateTime()));
+            }catch(Exception e){
+                System.out.println("I am in main: " + e);
+            }
         }
 }
